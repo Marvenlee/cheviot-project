fc763fec063d10f38627bc7edc4e5217f9221310
marven <marven10@gmail.com>
2024-06-02 18:05:44 +0100

    Partition sizes are now set in setup-env.sh script.

--- setup-env.sh
+++ setup-env.sh
@@ -4,8 +4,13 @@
 #
 
 export PATH=$PATH:$PWD/build/native/bin
+export PSEUDO_PREFIX=${PWD}/build/native/
 
 export BOARD=raspberrypi4
 
-export PSEUDO_PREFIX=${PWD}/build/native/
+# boot partition (fat)
+export BOOT_PARTITION_SIZE_MB=16
+
+# root partition (ext2)
+export ROOT_PARTITION_SIZE_MB=128
 
